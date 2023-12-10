c077b2481f25b036a35ffb7a1eae0789ba68b1e9
unknown <Safayet Ahmed>
2023-12-10 01:53:36 -0800

    added package

---
+++ client/src/components/route/Route.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom";
+
+
+const Route = () => {
+    return (
+        <div>
+            <Outlet></Outlet>
+            kd
+        </div>
+    );
+};
+
+export default Route;
