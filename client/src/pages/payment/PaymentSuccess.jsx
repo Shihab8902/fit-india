e4e51e9730ec5aa31ca01b9023a72d41fffc8ac4
Shihab Hasan <shihabbd018@gmail.com>
2024-01-11 23:22:10 +0600

    added payment success page and api for handling successful payment status

--- client/src/pages/payment/PaymentSuccess.jsx
+++ client/src/pages/payment/PaymentSuccess.jsx
@@ -1,14 +1,37 @@
-import { useParams } from "react-router-dom"
+import { useNavigate, useParams } from "react-router-dom";
+
+import Confetti from 'react-confetti'
+
+import thumbs from '../../assets/images/thumbs.png';
+
 
 const PaymentSuccess = () => {
 
-    const tran_id = useParams();
-    console.log(tran_id);
+    const navigate = useNavigate();
+
+    const tranId = useParams().id;
 
 
     return (
-        <div>PaymentSuccess</div>
-    )
-}
+        <div>
 
-export default PaymentSuccess
+            <Confetti
+                width={window.innerWidth || 500}
+                height={window.innerHeight || 500}
+            />
+
+            <div className="flex items-center justify-center h-screen bg-[#A9A9A9]">
+                <div className="bg-white p-8 rounded shadow-lg text-center">
+                    <img src={thumbs} className="h-16 w-16 mx-auto" alt="" />
+                    <h2 className="text-2xl font-semibold mb-4">Order placed!</h2>
+                    <p className="text-gray-600">
+                        Thank you for your purchase. <br /> <span className="text-xs"> Your transaction was successful with transaction id: {tranId}.</span>
+                    </p>
+                    <button className="bg-[#444852] my-10 text-white w-full rounded-lg btn hover:text-black  py-3">Track your order</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default PaymentSuccess;
